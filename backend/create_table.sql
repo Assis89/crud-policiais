582b9ec60258945962ec52a90f7468837bbd44da
thiago de assis da silva <thiago.dasilva@cicc.local>
2025-08-25 10:33:50 -0300

    banco-rodando

--- backend/create_table.sql
+++ backend/create_table.sql
@@ -1,11 +1,13 @@
--- Script de criação da tabela policiais
+CREATE DATABASE CadastroPoliciais;
+USE CadastroPoliciais;
+
 CREATE TABLE policiais (
     id INT AUTO_INCREMENT PRIMARY KEY,
     rg_civil VARCHAR(20) NOT NULL UNIQUE,
     rg_militar VARCHAR(20) NOT NULL UNIQUE,
     cpf VARCHAR(14) NOT NULL UNIQUE,
     data_nascimento VARBINARY(255) NOT NULL,
-    matricula VARBINARY(255) NOT NULL
+    matricula VARCHAR(50)
 );
 
 -- Índices para otimizar consultas por CPF e matrícula
